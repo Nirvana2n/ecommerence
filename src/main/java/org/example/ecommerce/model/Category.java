598886da8ef6c472a2c0a7b0dccace4b1e3d1382
Nirvana <2dadash@gmail.com>
2025-02-14 12:09:34 +0330

    first step for ecommerce

---
+++ src/main/java/org/example/ecommerce/model/Category.java
@@ -0,0 +1,14 @@
+package org.example.ecommerce.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Category {
+    private Long categoryId;
+    private String categoryName;
+
+}
